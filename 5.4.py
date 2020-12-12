f7417662d6dafe57644c3b04b22bcd8f3eddb6a2
Ihtisyam <etsyam.addin@gmail.com>
2020-12-12 18:09:51 +0000

    simple file storage

--- 5.4.py
+++ 5.4.py
@@ -7,14 +7,14 @@ port = 8888
 s.connect(('192.168.56.102', port))
 
 file = open("sample.txt", "rb")
-SendData = file.read(1024)
+SendFile = file.read(1024)
 
-while SendData:
+while SendFile:
     data = s.recv(1024)
     print(data)
     
-    s.send(SendData)
-    SendData = file.read(1024)     
+    s.send(SendFile)
+    SendFile = file.read(1024)     
     
 s.close()
 
